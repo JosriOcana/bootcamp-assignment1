0fea591049ac5388acc15d1fd49d6d5255ddf2aa
JosriOcana <josri.ocana@aclcbutuan.edu.ph>
2025-02-22 09:56:52 +0800

    first commit

---
+++ activity1.py
@@ -0,0 +1,6 @@
+# Asking for user's first and last name
+first_name = input("What is your first name? ")
+last_name = input("What is your last name? ")
+
+# Printing a greeting message
+print(f"Hello, {first_name} {last_name}!")
